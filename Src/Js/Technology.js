4d6e0a67d02119d4fea50401d8d8c3ce8acd12ca
Raymond Gonzalez <raymondgon7@gmail.com>
2017-03-09 11:46:49 -0500

    Update Technology.js

--- Src/Js/Technology.js
+++ Src/Js/Technology.js
@@ -2,9 +2,43 @@ function Technology(tpn,ad,e,cd){
 	this.Technology = '';
 	this.TechName = tpn;
 	this.abilityDesc = ad;
+	this.type = '';
 	this.energy = e;
 	this.coolDown = cd;
-  this.Technology0 = [];	
+	this.active = false;
+   	this.Technology0 = [];	
+   
+   	 this.getType = function(){
+    	   this.types = ['LargePerm','SmallPerm','FieldPerm','UnitAffectPerm','Temporary','LastAFewTurns','EasilyBroken','Invincible'];
+       
+   	 };
+    
+   	 this.SetCheckType = function(i){
+    	     if(i >= 0){
+    		  return this.getType[i];
+    	     }   	
+  	  };
+   	 this.isPermenant = function(){
+    	  if(this.type !== ''){
+     	  if(this.type === 'LargePerm' || 'SmallPerm' || 'FieldPerm' ||'UnitAffectPerm'){
+    			
+     	  }else{
+    			
+    	   }
+    	 }
+    	};
+   	 this.lockEnergy = function(){
+    	    var e = this.energy;
+   	 	  while(this.active === false){
+    		        this.energy = e;
+    		  }
+    	};
+    
+   	 this.OnOrOff = function(){
+   	     if(this.active){this.active = true;}
+   		    else{this.active = false}
+    	    
+   	 };
 
 	return {'Technology ':'',
 	        'TechName':this.spellName,
@@ -12,7 +46,7 @@ function Technology(tpn,ad,e,cd){
 	        'abilityDesc':this.abilityDesc,
 	        'energy':this.energy,
 	        'coolDown':this.coolDown,
-          'Technology0':this.Technology0
+       	        'Technology0':this.Technology0
 		
 	};
 }
