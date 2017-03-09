fc55e4e199a47b9c3a103ded36e0a217873aa4c2
Raymond Gonzalez <raymondgon7@gmail.com>
2017-03-09 14:31:55 -0500

    Update Spell.js

--- Src/Js/Spell.js
+++ Src/Js/Spell.js
@@ -5,58 +5,69 @@ function Spell(spn,ad,e,cd){
 	this.consumption = e;
 	this.coolDown = cd;
 	this.attachedTo = '';
-	
+	 // attach to a unit 
 	this.attachTo = function(u,a){
-	   if(typeof u === 'object'){
-	   	
-		if(u.Unit || u.Technology){
-	     	u.attachTo = a;
-	     	this.attachedTo = a;
-	     	u.hasAttachment = true;
-	     	while(u.hasAttachment){
-	     	  if(u.Unit){
-	     	  if(u.hp < 0){
-	     		   u.hasAttachment = false;
-	     		   u.attachTo = null;
-	     		if(u.attachTo  === null){
-	     			 a === null;
-	     			 this.attachedTo = null;
-	     			 break;
-	     	  	 }
+		var attachmentCheck = function(u){
+			  
+			    u.hasAttachment = false;
+	            u.attachTo = null;
+	     	if(u.attachTo  === null){
+	     		 a === null;
+	    		 this.attachedTo = null;
+	     		   return true;
+	     	 }
+		  };
+	 if(typeof u === 'object'){
+	   	if(a.Unit || a.Technology){
+	   		 u.attachedTo = a;
+	   		 a.hasAttachment = true;
+	   		 a.attachment = u.spellName;
+	     	
+	     	if(a.hasAttachment === true){
+	     	  if(a.Unit){
+	     	  if(a.hp < 0){
+	     		if(attachmentCheck(a)){}//break}
 	    	    }
 	       	  }
-	      	 if(u.Technology){
-	     	  if(u.energy <= 0){
-	     		   u.hasAttachment = false;
-	     		   u.attachTo = null;
-               if(u.attachTo === null){
-               	     a === null;
-               	     this.attachedTo = null;
-               	     break;
-               }	     	
-	     	 }
+	      	 if(a.Technology){
+	     	  if(a.consumption <= 0){
+	     	   	if(attachmentCheck(a)){}// break}
+	    	 }
 	        }
-	        /*
 	        if(u.Spell){
 	     	  if(u.energy <= 0){
-	     		   u.hasAttachment = false;
-	     		   u.attachTo = null;
-               if(u.attachTo === null){
-               	     a === null;
-               	     break;
-               }	     	
+	     	      	attachmentCheck(a);
 	     	 }
 	        }
-	        */
-	     }
-		}
+	     } 
+		}else{return 'This Card cannot be attached';}
 	   }
+	   
 	};
 	this.isEquipment = function(){
 		   
-		
-		
 	};
+	this.deEquip = function(u,h){
+	 var b = u.attachTo;
+	  if(typeof u === 'object'){
+		   if(u.hasAttachment){
+		   	h.attachedTo = null;
+		   	u.hasAttachment = false;
+		    u.attachment = '';
+		   	
+		   }
+	 }	
+		   return b;
+	}
+	
+	this.switchEquipment = function(u,newTarget){
+	    if(u.hasAttachment){
+	       newTarget.attachment = u.attachment;
+	       u.hasAttachment = false;
+	       newTarget.hasAttachment = true;
+	    }
+	}
+	
 	this.assignMagicType = function(i){
 		if(i > 8){
 		  this.magicTypes = ['EQUIP','SPEEDSPELL','SLOWSPELL','PERMENANT','HIDDEN','FIELD','DARK','POSSESSION'];
@@ -68,7 +79,7 @@ function Spell(spn,ad,e,cd){
 	this.isEquipped = function(){
 	  if(this.spellWornOff){
  	   if(this.attachedTo !== null || undefined){
-		  	 	return true;
+		    return true;
 	 	 }
 	 }
 	   	return false;
