90227ea2a039194cfc8c36d25b024998b437e098
Raymond Gonzalez <raymondgon7@gmail.com>
2017-03-31 14:08:54 -0400

    Update Technology.js

--- Src/Js/Technology.js
+++ Src/Js/Technology.js
@@ -6,47 +6,43 @@ function Technology(tpn,ad,e,cd){
 	this.energy = e;
 	this.coolDown = cd;
 	this.active = false;
-   	this.Technology0 = [];	
-   
-   	 this.getType = function(){
-    	   var types = ['LargePerm','SmallPerm','FieldPerm','UnitAffectPerm','Temporary','LastAFewTurns','EasilyBroken','Invincible'];
-       	    return types[i];
-   	 };
+	this.isPermenant = false;
+    this.Technology0 = [];	
     
-   	 this.SetCheckType = function(i){
-    	     if(i >= 0){
-    		  return this.getType[i];
-    	     }   	
-  	  };
-   	 this.isPermenant = function(){
-    	  if(this.type !== ''){
-     	  if(this.type === 'LargePerm' || 'SmallPerm' || 'FieldPerm' ||'UnitAffectPerm'){
-    			
-     	  }else{
-    			
-    	   }
-    	 }
-    	};
-   	 this.lockEnergy = function(){
-    	    var e = this.energy;
-   	 	  while(this.active === false){
-    		        this.energy = e;
-    		  }
-    	};
+    this.getType = function(g){
+       var types = ['LargePerm','SmallPerm','FieldPerm','UnitAffectPerm','Temporary','LastAFewTurns','EasilyBroken','Invincible'];
+          return types[g];
+    };
     
-   	 this.OnOrOff = function(){
-   	     if(this.active){this.active = true;}
-   		    else{this.active = false}
-    	    
-   	 };
-
-	return {'Technology ':'',
-	        'TechName':this.spellName,
-	        'type':this.type,
-	        'abilityDesc':this.abilityDesc,
-	        'energy':this.energy,
-	        'coolDown':this.coolDown,
-       	        'Technology0':this.Technology0
-		
-	};
+    this.SetCheckType = function(g){
+         if(g >= 0){
+    	  return this.getType(g);
+         }   	
+    };
+    this.setTarget = function(board,x,y,castAbility){castAbility(board[x][y])}
+    this.castAbility = function(){
+    	
+    }
+    this.isPermenant = function(){
+      if(this.type !== ''){
+       if(this.type === 'LargePerm' || 'SmallPerm' || 'FieldPerm' ||'UnitAffectPerm'){
+    	  this.isPermenant = true;		
+       }else{
+    	  this.isPermenant = false;			
+       }
+     }
+    };
+    this.lockEnergy = function(){
+        var e = this.energy;
+    	 while(this.active === false){this.energy = e}
+    };
+    
+    this.OnOrOff = function(){
+        if(this.active){this.active = true;}
+   	    else{this.active = false}
+    };
+    
+    
+	return this;
 }
+
