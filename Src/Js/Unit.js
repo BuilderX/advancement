cec6ace6766010993c61a1f091715cd5a88002ba
Raymond Gonzalez <raymondgon7@gmail.com>
2017-03-31 14:09:15 -0400

    Update Unit.js

--- Src/Js/Unit.js
+++ Src/Js/Unit.js
@@ -1,18 +1,34 @@
+
 function Unit(upn,ad,e,cd){
 	this.Unit = 'Unit';
+	this.hp = 0;
+	this.def = 0;
+	this.abilities = [];
 	this.unitName = upn;
+	this.position = [0,0];
 	this.abilityDesc = ad;
-  this.type = '';
 	this.energy = e;
 	this.coolDown = cd;
-
-		return {
-           'Unit ':'',
-	         'unitName':this.unitName,
-           'type':this.type,
-	         'abilityDesc':this.abilityDesc,
-	         'energy':this.energy,
-	         'coolDown':this.coolDown
-		
-	};
+    
+      this.castAbility = function(i,obj){
+      	  var keys = Object.keys(obj);
+      	    return this.abilities[i];
+      }
+      this.setPosition = function(x,y){
+      	     this.position[0] = x;
+      	     this.position[1] = y;
+      }
+      this.getPosition = function(){
+      	    return this.position;
+      }
+      this.warp = function(x,y,map){
+      	var r = Math.random(0) * map.length - 1;
+      	if(map.length < x){
+      		    x = r;
+      		    this.setPosition(x,y);
+      		
+      	}
+      }
+     	return this;
 }
+
