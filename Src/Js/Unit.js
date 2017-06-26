ac0d9d7f36b3ce62220a1cddf801a1d368d03f18
Raymond Gonzalez <raymondgon7@gmail.com>
2017-06-26 16:54:50 -0400

    Update Unit.js

--- Src/Js/Unit.js
+++ Src/Js/Unit.js
@@ -13,7 +13,21 @@ function Unit(hp,att,def,abi,ty,upn,ad,e,cd){
 	this.occupied = false;
 	this.coolDown = cd;
 	this.currentTarget = '';
-    
+   
+	
+     if(ty !== undefined){ 
+	this.hp += type[ty]['hp'];
+	this.att += type[ty]['att'];
+	this.def += type[ty]['def'];
+	this.speed += type[ty]['speed'];
+        this.energy += type[ty]['energy'];
+       if(this.coolDown === undefined)
+          this.coolDown = 0;
+          this.coolDown += type[ty]['coolDown'];
+      } 	
+	
+	
+	
       this.castAbility = function(i,obj){
       	  var keys = Object.keys(obj);
       	    return this.abilities[i];
@@ -38,3 +52,14 @@ function Unit(hp,att,def,abi,ty,upn,ad,e,cd){
       }
      	return this;
 }
+
+var type = { 'Sand':{hp:-2,att:2,def:6,speed:10,energy:10,coolDown:4},
+			 'Forest':{hp:6,att:7,def:1,speed:3,energy:5,coolDown:-1},
+             'Cold':{hp:-10,att:9,def:2,speed:-2,energy:4,coolDown:-7},
+             'Plains':{hp:2,att:2,def:10,speed:3,energy:4,coolDown:-20},
+             'Mountain':{hp:-6,att:12,def:3,speed:3,energy:7,coolDown:-4},
+             'Aqua':{hp:14,att:0,def:-8,speed:8,energy:12,coolDown:-5},
+             'Swamp':{hp:10,att:1,def:3,speed:2,energy:6,coolDown:-10}}
+
+
+
