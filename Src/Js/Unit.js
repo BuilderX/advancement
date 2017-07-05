a58b03642e27fc061f66d07269c6b2f2b5db32b9
Raymond Gonzalez <raymondgon7@gmail.com>
2017-07-05 16:35:57 -0400

    Update Unit.js

--- Src/Js/Unit.js
+++ Src/Js/Unit.js
@@ -1,33 +1,52 @@
+var type = { 'Sand':{hp:-2,att:2,def:6,speed:10,energy:10,coolDown:4},
+			 'Forest':{hp:6,att:7,def:1,speed:3,energy:5,coolDown:-1},
+             'Cold':{hp:-10,att:9,def:2,speed:-2,energy:4,coolDown:-7},
+             'Plains':{hp:2,att:2,def:10,speed:3,energy:4,coolDown:-20},
+             'Mountain':{hp:-6,att:12,def:3,speed:3,energy:7,coolDown:-4},
+             'Aqua':{hp:14,att:0,def:-8,speed:8,energy:12,coolDown:-5},
+             'Swamp':{hp:10,att:1,def:3,speed:2,energy:6,coolDown:-10}};
 
-function Unit(hp,att,def,abi,ty,upn,ad,e,cd){
-	this.Unit = 'Unit';
+
+
+function Entities(hp,att,def,sped,abi,ty,upn,ad,e,cd){
+	this.Entities = 'Entities';
 	this.hp = hp;
 	this.att = att;
 	this.def = def;
+	this.speed = sped;
 	this.abilities = abi;
 	this.unitName = upn;
 	this.position = [0,0];
 	this.abilityDesc = ad;
 	this.type = ty;
+	this.equipment = null;
 	this.energy = e;
 	this.occupied = false;
 	this.coolDown = cd;
-	this.currentTarget = '';
+	this.currentTargets = [];
+	this.attachments = null;
+    this.hasAttachment = false;
+    
+    if(ty !== undefined){ 
+		this.hp += type[ty]['hp'];
+		this.att += type[ty]['att'];
+		this.def += type[ty]['def'];
+		this.speed += type[ty]['speed'];
+	    this.energy += type[ty]['energy'];
+	   if(this.coolDown === undefined)
+	    this.coolDown = 0;
+	    this.coolDown += type[ty]['coolDown'];
+    }
+    
    
-	
-     if(ty !== undefined){ 
-	this.hp += type[ty]['hp'];
-	this.att += type[ty]['att'];
-	this.def += type[ty]['def'];
-	this.speed += type[ty]['speed'];
-        this.energy += type[ty]['energy'];
-       if(this.coolDown === undefined)
-          this.coolDown = 0;
-          this.coolDown += type[ty]['coolDown'];
-      } 	
-	
-	
-	
+    
+    
+      this.setOpponentTarget = function(arr){
+    	for(var i = 0; i < arr.length;i++){
+    		
+        	 this.currentTargets.push(arr[i]);
+    	}
+      }
       this.castAbility = function(i,obj){
       	  var keys = Object.keys(obj);
       	    return this.abilities[i];
@@ -50,16 +69,24 @@ function Unit(hp,att,def,abi,ty,upn,ad,e,cd){
       		
       	}
       }
+      
+      
+	
+	this.isEquipment = function(){
+		   
+	};
+	this.deEquip = function(u,h){
+		
+		var b = u.attachTo;
+	 if(typeof u === 'object'){
+		   if(u.hasAttachment){
+		   	h.attachedTo = null;
+		   	u.hasAttachment = false;
+		    u.attachment = '';
+		   }
+	 }	
+		   return b;
+	}
+      
      	return this;
 }
-
-var type = { 'Sand':{hp:-2,att:2,def:6,speed:10,energy:10,coolDown:4},
-			 'Forest':{hp:6,att:7,def:1,speed:3,energy:5,coolDown:-1},
-             'Cold':{hp:-10,att:9,def:2,speed:-2,energy:4,coolDown:-7},
-             'Plains':{hp:2,att:2,def:10,speed:3,energy:4,coolDown:-20},
-             'Mountain':{hp:-6,att:12,def:3,speed:3,energy:7,coolDown:-4},
-             'Aqua':{hp:14,att:0,def:-8,speed:8,energy:12,coolDown:-5},
-             'Swamp':{hp:10,att:1,def:3,speed:2,energy:6,coolDown:-10}}
-
-
-
